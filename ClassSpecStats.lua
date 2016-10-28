fc7bcfb5a225ba47a7bf7786ddd435de1bb6994b
reglohpri <71389505+Reglohprie@users.noreply.github.com>
2016-10-28 18:09:36 +0000

    First Release

---
+++ ClassSpecStats.lua
@@ -0,0 +1,85 @@
+local _, vars = ...;
+local L = vars.L
+
+local name = "ClassSpecStats"
+
+stats_Frame = CreateFrame("Frame",stats_Frame,UIParent)
+
+function stats_Frame:CreateWin()
+    if PaperDollFrame:IsVisible() then
+        if not stats_Window then
+            stats_Window = CreateFrame("Frame",stats_Window,stats_Frame)
+            local f = stats_Frame
+			f:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+                    edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+                    tile = true, tileSize = 16, edgeSize = 16,
+                    insets = { left = 1, right = 1, top = 1, bottom = 1 }})
+            f:SetBackdropColor(0,0,0,1)
+            f:SetFrameStrata("TOOLTIP")
+            f:SetWidth(PaperDollFrame:GetWidth()-50)
+			f:SetHeight(45)
+
+			stats_txt = f:CreateFontString(nil,"OVERLAY","GameFontWhite")
+			local ft = stats_txt
+			ft:ClearAllPoints()
+			ft:SetAllPoints(stats_Frame)
+			ft:SetJustifyH("CENTER")
+			ft:SetJustifyV("CENTER")
+            f:ClearAllPoints()
+            f:SetPoint("BOTTOMRIGHT",PaperDollFrame,"TOPRIGHT",0,0)
+            f:SetParent(PaperDollFrame)
+            f:Show()
+        end
+        return true
+    end
+    return false
+end
+
+function stats_Frame:Update()
+    if GetSpecialization() == nil then
+          return false
+    end
+
+	local specID = select(1,GetSpecializationInfo(GetSpecialization()))
+
+    if stats_Frame:CreateWin() then
+        local s = stats_Table[specID]
+
+        if s then
+            s = gsub(s,"Strength","Str")
+            s = gsub(s,"Agility","Agi")
+            s = gsub(s,"Intellect","Int")
+            s = gsub(s,"Stamina","Sta")
+            s = gsub(s,"Versatility","Vers")
+
+			-- H.Sch For multiple language
+			s = gsub(s,"Crit", L["Crit"])
+			s = gsub(s,"Str", L["Str"])
+			s = gsub(s,"Agil", L["Agi"])
+			s = gsub(s,"Agi", L["Agi"])
+			s = gsub(s,"Stam", L["Sta"])
+			s = gsub(s,"Sta", L["Sta"])
+			s = gsub(s,"Vers", L["Vers"])
+			s = gsub(s,"Haste", L["Haste"])
+			s = gsub(s,"Mast", L["Mastery"])
+			-- H.Sch End for multiple language
+            stats_txt:SetText(s)
+        end
+    end
+end
+
+local f = stats_Frame
+f:RegisterEvent("SPELLS_CHANGED")
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, event, ...)
+    if event == "ADDON_LOADED" and ... == name then
+        stats_Frame:Update()
+        PaperDollFrame:HookScript("OnShow", function() stats_Frame:Update() end)
+		print(name.." loaded!")
+    end
+    if event == "SPELLS_CHANGED" then
+        if IsAddOnLoaded(name) then
+            stats_Frame:Update()
+        end
+    end
+end)
---
+++ locale.lua
@@ -0,0 +1,28 @@
+local _, vars = ...;
+local Ld, La = {}, {}
+local locale = GetLocale()
+
+vars.L = setmetatable({},{
+    __index = function(t, s) return La[s] or Ld[s] or rawget(t,s) or s end
+})
+
+-- Ld means default (english) if no translation found. So we don't need a translation for "enUS" or "enGB".
+Ld["Agi"] = "Agi"
+Ld["Crit"] = "Crit"
+Ld["Haste"] = "Haste"
+Ld["Int"] = "Int"
+Ld["Mastery"] = "Mastery"
+Ld["Sta"] = "Stam"
+Ld["Str"] = "Str"
+Ld["Vers"] = "Vers"
+
+if locale == "deDE" then do end
+	La["Agi"] = "Bewegl."
+	La["Haste"] = "Tempo"
+	La["Crit"] = "Krit"
+	La["Int"] = "Int"
+	La["Mastery"] = "Meistersch."
+	La["Sta"] = "Ausdauer"
+	La["Str"] = "St\195\164rke"
+	La["Vers"] = "Vielseitigk."
+end
