473ee2f2328de0c972323bcc6a1ec571a4a3d354
reglohpri <reglohpri@c097eb44-b718-4ee9-9ad6-c082f94c5b66>
2019-04-23 17:21:17 +0000

    Patch 8.1.5 Stat priorities for the following classes have been updated
    Havoc Demon Hunter DPS, Guardian Druid Tank
    Mistweaver Monk Healing, Windwalker Monk DPS
    Shadow Priest DPS, Protection Warrior Tank

--- trunk/stats.lua
+++ trunk/stats.lua
@@ -1,5 +1,5 @@
 local x = {}
-x["Version"] = "|cFFFAFA44Icy-Veins Stat Priorities:|cFF00EA00 14.03.2019|r"
+x["Version"] = "|cFFFAFA44Icy-Veins Stat Priorities:|cFF00EA00 23.04.2019|r"
 
 --[[ Deathknight Blood]]
 x[250] = "Item Level > Str > Vers > Haste > Crit > Mast"
@@ -13,7 +13,7 @@ x[102] = "Int > Haste > Mast > Crit > Vers"
 --[[ Druid Feral]]
 x[103] =  "Crit > Mast > Haste > Agi > Vers"
 --[[ Druid Guardian]]
-x[104] = "Survival: (Armor/Agi/Stam)>Mast>Vers>Haste>Crit \n DPS Boost: Agi > Haste >= Crit >= Vers >= Mast"
+x[104] = "Survival: (Armor/Agi/Stam)>Vers>Mast>Haste>Crit \n DPS Boost: Agi > Haste >= Crit >= Vers >= Mast"
 --[[ Druid Restoration]]
 x[105] = "Raid: Mast > (Haste = Crit = Vers) > Int \n Dungeon: (Mast = Haste) > Vers > Crit > Int"
 
@@ -34,7 +34,7 @@ x[64] = "Int > Crit 33.34% > Haste > Vers > Mast > Crit"
 --[[ Monk Brewmaster]]
 x[268] = "Agi > Mast > (Crit = Vers) > Haste"
 --[[ Monk Mistweaver]]
-x[270] = "Raid: Int > Crit > (Mast = Vers) > Haste \n Dungeon Mythic+: Int > (Mast >= Haste) > Vers > Crit"
+x[270] = "Raid: Crit > (Mast = Vers) > Int > Haste \n Dungeon Mythic+: Int > (Mast >= Haste) > Vers > Crit"
 --[[ Monk Windwalker]]
 x[269] = "Agi > Vers > Mast > Crit > Haste"
 
@@ -50,7 +50,7 @@ x[256] = "Int > Haste > Crit > Vers > Mast"
 --[[ Priest Holy]]
 x[257] = "Raid: Int > (Mast = Crit) > Vers > Haste \n Dungeon: Int > Crit > Haste > Vers > Mast"
 --[[ Priest Shadow]]
-x[258] = "(Haste = Crit) > Int > Mast > Vers"
+x[258] = "(Haste = Crit) > (Mast = Vers) > Int"
 
 --[[ Rogue Assassination]]
 x[259] = "Agi > Haste > Crit > Mast > Vers"
@@ -78,10 +78,10 @@ x[71] = "Haste > Crit > Vers > Mast > Str"
 --[[ Warrior Fury]]
 x[72] = "Crit > Mast > Haste > Vers > Str"
 --[[ Warrior Protection]]
-x[73] = "Haste > Armor > Vers > Mast > Crit > Str"
+x[73] = "Haste > Vers > Mast > Crit > Str > Armor"
 
 --[[ Demon Hunter Havoc]]
-x[577] = "Agi > (Haste = Vers) > Crit > Mast"
+x[577] = "Vers > (Crit = Haste) > Agi > Mast"
 --[[ Demon Hunter Vengeance]]
 x[581] = "Agi > Haste > Vers > Mast > Crit"
 stats_Table = x
